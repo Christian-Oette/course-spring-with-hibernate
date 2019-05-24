46c84bf3f0c4cf533be31493bcc5b764af5daf15
chris <christian.oette@web.de>
2019-05-24 22:14:13 +0200

    Add initial project

---
+++ src/main/java/de/christianoette/course/SpringAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package de.christianoette.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndHibernateApplication.class, args);
+	}
+
+}
