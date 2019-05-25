05233f791c1ebb60eff88e9dbf3f69aceea20bfe
chris <christian.oette@web.de>
2019-05-25 16:37:07 +0200

    Change project to junit jupiter

--- src/test/java/de/oette/course/A01/AutowiredAndInjectTest.java
+++ src/test/java/de/oette/course/A01/AutowiredAndInjectTest.java
@@ -1,16 +1,13 @@
 package de.oette.course.A01;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class AutowiredAndInjectTest {
+class AutowiredAndInjectTest {
 
-	@Test
-	public void testAnnotations() {
+    @Test
+    void testAnnotations() {
 		// Start here!
 	}
 
---
+++ src/test/java/de/oette/course/A01/PureUnitTest.java
@@ -0,0 +1,12 @@
+package de.oette.course.A01;
+
+
+import org.junit.jupiter.api.Test;
+
+public class PureUnitTest {
+
+    @Test
+    public void testWithoutSpring()  {
+
+    }
+}
