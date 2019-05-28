3484b0272890fdfc341050857354e2b24fa4831c
chris <christian.oette@web.de>
2019-05-28 21:09:49 +0200

    Add A02 content

--- src/test/java/de/oette/course/A02/ApplicationContextTest.java
+++ src/test/java/de/oette/course/A02/ApplicationContextTest.java
@@ -1,11 +1,21 @@
 package de.oette.course.A02;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericApplicationContext;
 
 class ApplicationContextTest {
 
     @Test
     void testInitSpring() {
+        GenericApplicationContext context = new GenericApplicationContext();
+        context.refresh();
+        context.registerBean(TestContext.class);
 
+        TestContext bean = context.getBean(TestContext.class);
+        System.out.println(bean);
+        System.out.println(bean);
+    }
+
+    private static class TestContext {
     }
 }
