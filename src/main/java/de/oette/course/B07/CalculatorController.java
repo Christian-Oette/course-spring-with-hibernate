7e90848f34cee1c3dc243fb35fdf1fd7b4c5f67f
chris <christian.oette@web.de>
2019-06-08 23:14:19 +0200

    Add B07

--- src/main/java/de/oette/course/B07/CalculatorController.java
+++ src/main/java/de/oette/course/B07/CalculatorController.java
@@ -1,10 +1,20 @@
 package de.oette.course.B07;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "/calculate")
 public class CalculatorController {
 
-    // TODO implement sum!
+    @GetMapping(value = "/sum")
+    public Integer sum(int x, int y) {
+        return x + y;
+    }
 
-    // TODO implement multiply!
+    @GetMapping(value = "/multiply")
+    public Integer multiply(int x, int y) {
+        return x * y;
+    }
 }
