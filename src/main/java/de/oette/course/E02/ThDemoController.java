1595ce766deb1a828c3fc4d8daad69860bffce0e
chris <christian.oette@web.de>
2019-06-12 19:37:19 +0200

    Add th:each

--- src/main/java/de/oette/course/E02/ThDemoController.java
+++ src/main/java/de/oette/course/E02/ThDemoController.java
@@ -4,17 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ThDemoController {
 
     @GetMapping(value = {"/", "index.html"})
     public ModelAndView index() {
-        CardData cardData = new CardData();
-        cardData.setTitle("Title");
-        cardData.setText("Text");
+        List<CardData> data = new ArrayList<>();
+        data.add(createCardData("Title", "Text"));
+        data.add(createCardData("Welcome", "to my Page"));
+        data.add(createCardData("Hello", "World"));
+
+
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("cardData", cardData);
+        modelAndView.addObject("cardData", data);
         return modelAndView;
+    }
+
+    private CardData createCardData(String title, String text) {
+        CardData cardData = new CardData();
+        cardData.setTitle(title);
+        cardData.setText(text);
+        return cardData;
     }
 
     @GetMapping(value = {"/contact", "contact.html"})
