3a6985bc5be02e9d84fb7991158f95b61aaf644f
chris <christian.oette@web.de>
2019-06-13 19:41:07 +0200

    Prepare F02

--- src/main/java/de/oette/course/F02/PropertyBundle.java
+++ src/main/java/de/oette/course/F02/PropertyBundle.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 /**
  * property.demo.inject
  * property.demo.environment-var
- * property.demo.subclass.value1
- * property.demo.subclass.value2
+ * property.demo.subCategory.value1
+ * property.demo.subCategory.value2
  */
 @Component
 public class PropertyBundle {
--- src/main/java/de/oette/course/SpringAndHibernateApplication.java
+++ src/main/java/de/oette/course/SpringAndHibernateApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SuppressWarnings("SpellCheckingInspection")
 @SpringBootApplication
-@ComponentScan(basePackages = "de.oette.course.F01")
+@ComponentScan(basePackages = "de.oette.course.F02")
 public class SpringAndHibernateApplication {
 
 	public static void main(String[] args) {
