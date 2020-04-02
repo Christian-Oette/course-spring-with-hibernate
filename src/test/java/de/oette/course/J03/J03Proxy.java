58ae61ca915e4acb5173a116e079e20e4e7c81b8
chris <christian.oette@web.de>
2020-04-02 16:17:55 +0200

    Rename ComponentProxy

--- src/test/java/de/oette/course/J03/J03Proxy.java
+++ src/test/java/de/oette/course/J03/J03Proxy.java
@@ -5,8 +5,7 @@ import org.springframework.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
 
-public class J03ComponentProxy implements MethodInterceptor {
-
+public class J03Proxy implements MethodInterceptor {
 
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
